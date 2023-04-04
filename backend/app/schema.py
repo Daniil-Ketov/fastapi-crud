117702c7bfa264d3ac286017ce3cf04d3373c69d
Ketov Daniil <ketov.daniil123@yandex.ru>
2023-04-04 13:12:53 +0300

    created schemas

---
+++ backend/app/schema.py
@@ -0,0 +1,39 @@
+from datetime import date
+from pydantic import BaseModel, validator
+from pydantic.generics import GenericModel
+from app.model.person import Sex
+from fastapi import HTTPException
+from typing import Optional, TypeVar, Generic, List
+
+
+T = TypeVar('T')
+
+
+class PersonCreate(BaseModel):
+    name: str
+    sex: Sex
+    birth_date: date
+    birth_place: str
+    country: str
+
+    # Sex validation
+    @validator("sex")
+    def sex_validation(cls, v):
+        if hasattr(cls, v) is False:
+            raise HTTPException(status_code=400, detail="Invalid input sex")
+        return v
+
+
+class ResponseSchema(BaseModel):
+    detail: str
+    result: Optional[T] = None
+
+
+class PageResponse(GenericModel, Generic[T]):
+    """ The response for a pagination query. """
+
+    page_number: int
+    page_size: int
+    total_pages: int
+    total_record: int
+    content: List[T]
