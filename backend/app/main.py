2b937c87adbf55ab3f2d24cc45496dcfc273abca
Ketov Daniil <ketov.daniil123@yandex.ru>
2023-04-04 13:26:53 +0300

    created controllers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from app.config import db
+
+
+origins = ["http://localhost:3000"]
 
 
 def init_app():
@@ -10,6 +14,14 @@ def init_app():
         title="CRUD app",
         description="CRUD sort pagination page",
         version="1"
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
     )
 
     @app.on_event("startup")
